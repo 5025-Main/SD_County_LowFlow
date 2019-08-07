c19857b2ad7a54a1d4485e0d55d94e7b3c710390
Luis De La Torre <luisdelatorre89@gmail.com>
2019-08-07 09:23:58 -0700

    updates to deliverable

--- Python Code/2-Create hydrograph from barocompensated data.py
+++ Python Code/2-Create hydrograph from barocompensated data.py
@@ -135,7 +135,7 @@ print ('')
 #%% START HERE - SITE NAME
 
 ## SITE NAME HERE #################
-SITE_YOU_WANT_TO_PROCESS = 'SWT-055'
+SITE_YOU_WANT_TO_PROCESS = 'SLR-045B'
 
 ### UPDATE HERE #####
 start, end = dt.datetime(2019,5,1,0,0), dt.datetime(2019,7,31,23,59)
