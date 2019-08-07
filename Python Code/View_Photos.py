45371f3a69877659069b758bee6c2b423a46bb0b
Alex Messina <atm1984@gmail.com>
2019-08-07 12:05:33 -0700

    added level and flow display

--- Python Code/View_Photos.py
+++ Python Code/View_Photos.py
@@ -21,7 +21,7 @@ flow_filename = [f for f in os.listdir(flow_dir) if f.split('-working draft.xlsx
 flow_df = pd.read_excel(flow_dir+flow_filename,sheetname=site+'-flow')
 level_dir = 'C:/Users/alex.messina/Documents/GitHub/SD_County_LowFlow/4 - Level calibration files and figures/Data Output 07_25_2019/'
 level_filename= [f for f in os.listdir(level_dir) if f.split('-calibration.xlsx')[0]==site][0]
-level_df = pd.read_excel(level_dir+level_filename,sheetname=site+'Level and Flow data')
+level_df = pd.read_excel(level_dir+level_filename,sheetname='Level and Flow data')
 
 pic_dir = 'C:/Users/alex.messina/Downloads/'
 pic_folder = site + '/'
@@ -30,7 +30,7 @@ pic_folder = site + '/'
 #%%
 
 # define your images to be plotted
-pics = [os.listdir(pic_dir+pic_folder)][0][950:] ## You can limit photos here
+pics = [os.listdir(pic_dir+pic_folder)][0][975:] ## You can limit photos here
 
 # now the real code :) 
 curr_pos = 0
@@ -55,8 +55,9 @@ def key_event(e):
     print 'Date taken: '+date_taken
     t = dt.datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S')
     t_round5 = dt.datetime(2019, t.month, t.day, t.hour,5*(t.minute // 5),0)
-    ## Get flow data at time of pic
+    ## Get flow and level data at time of pic
     flow_at_image = flow_df.ix[t_round5,'Flow compound weir (gpm)']
+    level_at_image = level_df.ix[t_round5,'offset_level_w_neg']
     
     ## Image
     ax1.cla()
@@ -69,21 +70,33 @@ def key_event(e):
     ## Plot flow data
     ax2.cla()
     ax2.plot_date(flow_df.index,flow_df['Flow compound weir (gpm)'],marker='None',ls='-',c='b',label='Flow compound weir (gpm)')
-    ax2.plot_date(t_round5, flow_at_image,marker='o',ls='None',c='r')
+    ax2.plot_date(t_round5, flow_at_image,marker='o',ls='None',c='r',label='Flow at picture='+"%.3f"%flow_at_image)
     
+    ## Plot Level data   
+    ax2_2.cla()
+    ax2_2.plot_date(level_df.index, level_df['offset_level_w_neg'],marker='None',ls='-',c='orange',label='Level (inches)')    
+    ax2_2.plot_date(t_round5, level_at_image,marker='o',ls='None',c='g',label='Level at picture='+"%.2f"%level_at_image)
+    ax2_2.set_ylim(-1, 6)
     ## Set plot limits
     ax2.set_xlim(t_round5 - dt.timedelta(hours=8), t_round5 + dt.timedelta(hours=8))
     ## Get flow data over a 24 hour surrounding period
     flow_over_interval = flow_df.ix[t_round5 - dt.timedelta(hours=8):t_round5 + dt.timedelta(hours=8),'Flow compound weir (gpm)']
     ## y limits
     if flow_over_interval.min() == 0. and flow_over_interval.max() > 0.:
-        ax2.set_ylim(-5.,flow_over_interval.max()*1.1)
+        ax2.set_ylim(-1.,flow_over_interval.max()*1.1)
     elif flow_over_interval.min() == 0. and flow_over_interval.max() == 0.:
-            ax2.set_ylim(-5.,5.)
+            ax2.set_ylim(-3.,3.)
     else:
         ax2.set_ylim(flow_over_interval.min()*0.9,flow_over_interval.max()*1.1)
         
     ax2.xaxis.set_major_formatter(mpl.dates.DateFormatter('%A \n %m/%d/%y %H:%M'))
+
+    ax2.set_ylabel('Flow (gpm)'), ax2_2.set_ylabel('Level (inches)')
+
+    ## Legends, they're all around
+    ax2.legend(loc='upper left')
+    ax2_2.legend(loc='upper right')
+        
     fig.canvas.draw()
     print 'done'
     
@@ -100,6 +113,7 @@ date_taken = Image.open(picture_file)._getexif()[36867]
 t = dt.datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S')
 t_round5 = dt.datetime(2019, t.month, t.day, t.hour,5*(t.minute // 5),0)
 flow_at_image = flow_df.ix[t_round5,'Flow compound weir (gpm)']
+level_at_image = level_df.ix[t_round5,'offset_level_w_neg']
 
 ## Image
 ax1 = plt.subplot(211)
@@ -111,20 +125,34 @@ plt.show()
 
 ax2 = plt.subplot(212)
 ax2.plot_date(flow_df.index,flow_df['Flow compound weir (gpm)'],marker='None',ls='-',c='b',label='Flow compound weir (gpm)')
-ax2.plot_date(t_round5, flow_at_image,marker='o',ls='None')
+ax2.plot_date(t_round5, flow_at_image,marker='o',ls='None',label='Flow at picture='+"%.3f"%flow_at_image)
 
+
+## Level
+ax2_2 = ax2.twinx()
+ax2_2.plot_date(level_df.index, level_df['offset_level_w_neg'],marker='None',ls='-',c='orange',label='Level (inches)')
+ax2_2.plot_date(t_round5, level_at_image,marker='o',ls='None',c='g',label='Level at picture='+"%.2f"%level_at_image)
+ax2_2.set_ylim(-1, 6)
+
+## Legends, they're all around
+ax2.legend(loc='upper left')
+ax2_2.legend(loc='upper right')
+
+## Y labels
+ax2.set_ylabel('Flow (gpm)'), ax2_2.set_ylabel('Level (inches)')
+
+
+## x and y limits
 ax2.set_xlim(t_round5 - dt.timedelta(hours=8), t_round5 + dt.timedelta(hours=8))
 flow_over_interval = flow_df.ix[t_round5 - dt.timedelta(hours=8):t_round5 + dt.timedelta(hours=8),'Flow compound weir (gpm)']
 if flow_over_interval.min() == 0. and flow_over_interval.max() > 0.:
     ax2.set_ylim(-5.,flow_over_interval.max()*1.1)
 elif flow_over_interval.min() == 0. and flow_over_interval.max() == 0.:
-        ax2.set_ylim(-5.,5.)
+        ax2.set_ylim(-3.,3.)
 else:
     ax2.set_ylim(flow_over_interval.min()*0.9,flow_over_interval.max()*1.1)
+## X axis date format
 ax2.xaxis.set_major_formatter(mpl.dates.DateFormatter('%A \n %m/%d/%y %H:%M'))
-
-ax2_2 = ax2.twinx()
-ax2
 
 
 plt.tight_layout()
